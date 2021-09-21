c479d58910731a3161a8f909afb72ba0d537f8d2
drmocellin52@hotmail.com <Denise Mocellin>
2021-09-20 21:36:10 -0300

    Criado as pastas model, controller e um arquivo ContaBancaria

---
+++ spring02/src/main/java/com/projetofinal/spring02/model/ContaBancaria.java
@@ -0,0 +1,19 @@
+package com.projetofinal.spring02.model;
+
+public package com.projetofinal.spring02.model;
+
+    public class ContaBancaria {
+        private int numero;
+        private int agencia;
+        private double saldo;
+        private String titular;
+        private int tipoConta;
+
+    public ContaBancaria(int numero, int agencia, double saldo, String titular, int tipoConta) {
+    this.numero = numero;
+    this.agencia = agencia;
+    this.saldo = saldo;
+    this.titular = titular;
+    this.tipoConta = tipoConta;
+
+    }
