f6567fb6d8aa8b943fb17e5b590a114fd0f04e77
LucasEduardo <lucas.cravo@hotmail.com>
2021-09-20 21:47:58 -0300

    Arrumando o arquivo ContaBancaria

--- spring02/src/main/java/com/projetofinal/spring02/model/ContaBancaria.java
+++ spring02/src/main/java/com/projetofinal/spring02/model/ContaBancaria.java
@@ -1,19 +1,20 @@
 package com.projetofinal.spring02.model;
-
-public package com.projetofinal.spring02.model;
-
     public class ContaBancaria {
+
         private int numero;
         private int agencia;
         private double saldo;
         private String titular;
         private int tipoConta;
 
-    public ContaBancaria(int numero, int agencia, double saldo, String titular, int tipoConta) {
-    this.numero = numero;
-    this.agencia = agencia;
-    this.saldo = saldo;
-    this.titular = titular;
-    this.tipoConta = tipoConta;
+        public ContaBancaria(int numero, int agencia, double saldo, String titular, int tipoConta) {
+
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = saldo;
+        this.titular = titular;
+        this.tipoConta = tipoConta;
+
+        }
 
     }
