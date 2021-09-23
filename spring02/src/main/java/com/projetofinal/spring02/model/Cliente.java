74dc750187b8104b86d9ca35b3eb1c1e6a6f39ad
Lucaaas7 <lucas.cravo@hotmail.com>
2021-09-22 22:12:38 -0300

    Teste

--- spring02/src/main/java/com/projetofinal/spring02/model/Cliente.java
+++ spring02/src/main/java/com/projetofinal/spring02/model/Cliente.java
@@ -7,13 +7,14 @@ public class Cliente {
        private String telefone;
        private String email;
 
-       public Cliente(int codigo, String nome, int cpf, String telefone, String email) {
-            this.codigo = codigo;
-            this.nome = nome;
-            this.cpf = cpf;
-            this.telefone = telefone;
-            this. email = email;
-       }
-       
+     public Cliente(int codigo, String nome, int cpf, String telefone, String email) {
+          this.codigo = codigo;
+          this.nome = nome;
+          this.cpf = cpf;
+          this.telefone = telefone;
+          this. email = email;
+     }
+
+     
            
 }
