603e0795f980e271e71938cfe8ee3b22038519a6
Denise Mocellin <83514023+denisemocellin52@users.noreply.github.com>
2021-09-23 09:16:21 -0300

    Update ContaBancaria.java

--- spring02/src/main/java/com/projetofinal/spring02/model/ContaBancaria.java
+++ spring02/src/main/java/com/projetofinal/spring02/model/ContaBancaria.java
@@ -18,3 +18,44 @@ package com.projetofinal.spring02.model;
         }
 
     }
+
+public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public int getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(int tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+}
